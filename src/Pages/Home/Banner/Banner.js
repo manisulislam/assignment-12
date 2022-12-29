bdd43b02682b8bd29b817fca797802d8d4a0d384
anisul islam <anisulislam980340@gmail.com>
2022-12-29 10:28:03 +0600

    completely setup banner in the home page

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import image from '../../../assets/Bannerwatch.jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <h3>this is banner</h3>
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${image})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Your values and your personal style</h1>
+                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import SectionOne from '../SectionOne/SectionOne';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <SectionOne></SectionOne>
         </div>
     );
