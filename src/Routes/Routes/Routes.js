c03847207d1205f8c96f5b51db9318cc217fe6df
anisul islam <anisulislam980340@gmail.com>
2022-12-29 09:39:15 +0600

    setup navbar and routes

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Blog from "../../Pages/Blog/Blog";
+import Home from "../../Pages/Home/Home/Home";
+import LogIn from "../../Pages/LogIn/LogIn";
+import Register from "../../Pages/Register/Register";
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children:[
+            {
+                path:'/',
+                element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <LogIn></LogIn>
+            },
+            {
+                path: '/register',
+                element:<Register></Register>
+            }
+        ]
+        
+    }
+])
+export default routes;
