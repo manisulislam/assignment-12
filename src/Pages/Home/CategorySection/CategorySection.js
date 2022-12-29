4738bda2b0b69213dea3bfe1ed9134637969d906
anisul islam <anisulislam980340@gmail.com>
2022-12-29 15:48:10 +0600

    setup category three section

---
+++ src/Pages/Home/CategorySection/CategorySection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CategorySection = () => {
+    return (
+       
+             <div >
+                <h1 className='text-orange-500  text-2xl text-center font-semibold'>OUR THREE BRAND CATEGORY</h1>
+               <div className='flex justify-center my-12'>
+               <button className="btn btn-outline btn-secondary mr-8">ROLEX</button>
+                <button className="btn btn-outline btn-secondary mr-8 ">OMEGA</button>
+                <button className="btn btn-outline btn-secondary mr-8">CARTIER</button>
+
+               </div>
+            </div>
+            
+    
+    );
+};
+
+export default CategorySection;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import CategorySection from '../CategorySection/CategorySection';
 import SectionOne from '../SectionOne/SectionOne';
 
 const Home = () => {
     return (
-        <div>
+        <div >
             <Banner></Banner>
             <SectionOne></SectionOne>
+            <CategorySection></CategorySection>
+           
         </div>
     );
 };
