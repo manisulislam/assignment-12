e0db7c5e24f660fdac97b2191024480e65537406
anisul islam <anisulislam980340@gmail.com>
2022-12-29 09:57:35 +0600

    setup 404 error page

---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import image from '../../assets/errorpage.jpg'
+
+const ErrorPage = () => {
+    return (
+        <div>
+           
+            <img className='w-96 mx-auto' src={image} alt="" />
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Blog from "../../Pages/Blog/Blog";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
 import LogIn from "../../Pages/LogIn/LogIn";
 import Register from "../../Pages/Register/Register";
@@ -28,6 +29,11 @@ const routes = createBrowserRouter([
             }
         ]
         
+    },
+    {
+        path:'*',
+        element: <ErrorPage></ErrorPage>
     }
+
 ])
 export default routes;
